09ebdad61ae6ef3af55a22ce68be1c35b358c183
bentleylewis <bbentleylewis@gmail.com>
2025-04-07 00:39:46 -0500

    operator buttons, let numString, x, y, operator

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+let numString = "";
+let x = null;
+let y = null;
+let operator = null;
+
+const calcDisplay = document.getElementById("displayText");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+
+numberButtons.forEach(button => {
+  button.addEventListener ("click", () => {
+    const value = button.textContent;
+    numString += value;
+    calcDisplay.textContent = numString;
+    operatorButtons.forEach(btn => btn.classList.remove("activeOperator"));
+
+  });
+});
+
+
+
+operatorButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    operatorButtons.forEach(btn => btn.classList.remove("activeOperator"));
+    button.classList.add("activeOperator");
+
+    operator = button.textContent;
+
+    if (numString !== "") { 
+      if (x === null) {
+        x = numString;
+        numString = "";
+      } 
+      else {
+        y = numString;
+      }
+    }
+    });
+  });
+
+
+
 function operate(x, y, operator) {
     switch(operator) {
       case "+":
